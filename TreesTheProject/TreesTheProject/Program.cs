cbb48bcf08db29b1682966fdb92ac6478f7b3c8a
GMR Student <gmrstudent@outlook.com>
2022-03-11 18:16:41 -0800

    yes

--- TreesTheProject/TreesTheProject/Program.cs
+++ TreesTheProject/TreesTheProject/Program.cs
@@ -6,7 +6,7 @@ namespace TreesTheProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ TreesTheProject/TreesTheProject/Tree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreesTheProject
+{
+    class Node<T>
+    {
+        public Node<T> Parent;
+        public Node<T> LeftChild;
+        public Node<T> RightChild;
+        public bool hasTwoChildren
+        {
+            get
+            {
+                return LeftChild != null && RightChild != null;
+            }
+        }
+        public T Value;
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+
+    class Tree<T>
+    {
+        public Node<T> Root;
+        public void Insert(T value)
+        {
+
+            if(Root == null)
+            {
+                Root = new Node<T>(value);
+                
+                return;
+            }
+        }
+    }
+}
