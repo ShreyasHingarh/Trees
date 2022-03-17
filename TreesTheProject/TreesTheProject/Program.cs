6b3d369fc88877108478f73767579315e360f406
GMR Student <gmrstudent@outlook.com>
2022-03-16 18:15:27 -0700

    made insert and minimum

--- TreesTheProject/TreesTheProject/Program.cs
+++ TreesTheProject/TreesTheProject/Program.cs
@@ -6,7 +6,15 @@ namespace TreesTheProject
     {
         static void Main(string[] args)
         {
-            
+            Tree<int> tree = new Tree<int>();
+            int[] nums = new int[] {9,11,4,1,6,14};
+            for(int i = 0;i < nums.Length;i++)
+            {
+                tree.Insert(nums[i]);
+            }
+            ;
+
+
         }
     }
 }
--- TreesTheProject/TreesTheProject/Tree.cs
+++ TreesTheProject/TreesTheProject/Tree.cs
@@ -23,18 +23,66 @@ namespace TreesTheProject
         }
     }
 
-    class Tree<T>
+    class Tree<T> where T : IComparable<T>
     {
         public Node<T> Root;
+        
+        public int size;
+        public T Minimum 
+        {
+            get
+            {
+                Node<T> temp = Root;
+                do
+                {
+                    temp = temp.LeftChild;
+                } while (!temp.LeftChild.Equals(null));
+                return temp.Value;
+            }
+        }
         public void Insert(T value)
         {
 
             if(Root == null)
             {
                 Root = new Node<T>(value);
-                
+                size++;
                 return;
             }
+            Node<T> temp = Root;
+            int count = 0;
+            do
+            {
+
+
+                if (temp.hasTwoChildren)
+                {
+                    if (value.CompareTo(temp.Value) > 0)
+                    {
+                        temp = temp.RightChild;
+                    }
+                    else if (value.CompareTo(temp.Value) <= 0)
+                    {
+                        temp = temp.LeftChild;
+                    }
+                }
+                else
+                {
+                    if (value.CompareTo(temp.Value) > 0)
+                    {
+                        temp.RightChild = new Node<T>(value);
+                        size++;
+                        return;
+                    }
+                    else if (value.CompareTo(temp.Value) <= 0)
+                    {
+                        temp.LeftChild = new Node<T>(value);
+                        size++;
+                        return;
+                    }
+                }
+                count++;
+            } while (count < size);
         }
     }
 }
